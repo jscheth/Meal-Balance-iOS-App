52a28f81e4b9eb0477655864f049ebc0afb05d3a
jscheth <sovichea.chet@gmail.com>
2025-07-01 15:31:13 -0700

    Initial Commit

---
+++ Meal Balance/Meal_BalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_BalanceApp.swift
+//  Meal Balance
+//
+//  Created by Jonathan Cheth on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Meal_BalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Meal BalanceTests/Meal_BalanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_BalanceTests.swift
+//  Meal BalanceTests
+//
+//  Created by Jonathan Cheth on 7/1/25.
+//
+
+import Testing
+@testable import Meal_Balance
+
+struct Meal_BalanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
